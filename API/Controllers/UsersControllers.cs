26cbaaa22fbf5bfa232a950e394aff64c23a30f2
MADMVX <dskato0603@gmail.com>
2022-04-07 22:19:11 -0500

    part 2

--- API/Controllers/UsersControllers.cs
+++ API/Controllers/UsersControllers.cs
@@ -23,7 +23,7 @@ namespace API.Controllers
 
         //We make the methods async to do the request multithread
         //
-        //
+        // GET PETITONS
 
         [HttpGet]
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
@@ -37,10 +37,26 @@ namespace API.Controllers
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
             //Function to get entity by ID
-            return  await dataContext.Users.FindAsync(id);
+            var userItem = await dataContext.Users.FindAsync(id);
+            if(userItem == null){
+                return NotFound();
+            }
+            return userItem;
         }
 
 
+        // POST PETITION
+        [HttpPost]
+        public async Task<ActionResult<AppUser>> addUser(AppUser appUser){
+
+            //Add and save
+            await dataContext.Users.AddAsync(appUser);
+            await dataContext.SaveChangesAsync();
+
+            //This returns a StatusCodes.Status201Created response
+            return CreatedAtAction(nameof(GetUser), new {id = appUser.Id}, appUser);
+
+        }
 
     }
 }
