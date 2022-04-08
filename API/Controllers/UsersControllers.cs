a5529ba1a050fa33234742382217c367d356138d
MADMVX <dskato0603@gmail.com>
2022-04-07 23:16:08 -0500

    crud added for users controllers

--- API/Controllers/UsersControllers.cs
+++ API/Controllers/UsersControllers.cs
@@ -29,7 +29,7 @@ namespace API.Controllers
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _dataContext.Users.ToListAsync();
-            
+
         }
 
         //Get user by id - 
@@ -38,7 +38,8 @@ namespace API.Controllers
         {
             //Function to get entity by ID
             var userItem = await _dataContext.Users.FindAsync(id);
-            if(userItem == null){
+            if (userItem == null)
+            {
                 return NotFound();
             }
             return userItem;
@@ -47,14 +48,59 @@ namespace API.Controllers
 
         // POST PETITION
         [HttpPost]
-        public async Task<ActionResult<AppUser>> addUser(AppUser appUser){
+        public async Task<ActionResult<AppUser>> AddUser(AppUser appUser)
+        {
 
             //Add and save
             await _dataContext.Users.AddAsync(appUser);
             await _dataContext.SaveChangesAsync();
 
             //This returns a StatusCodes.Status201Created response
-            return CreatedAtAction(nameof(GetUser), new {id = appUser.Id}, appUser);
+            return CreatedAtAction(nameof(GetUser), new { id = appUser.Id }, appUser);
+
+        }
+
+
+        // PUT PETITION
+        // With PUT method we have to send again all the properties of the entity
+        // If we just want to update an specific property we have to user PATCH method
+        [HttpPut("{id}")]
+        public async Task<ActionResult<AppUser>> UpdateUser(int id, AppUser appUser)
+        {
+
+            if (id != appUser.Id)
+            {
+                return BadRequest();
+            }
+
+            //update and change entity state
+            _dataContext.Entry(appUser).State = EntityState.Modified;
+            await _dataContext.SaveChangesAsync();
+
+            //Just return a not content after update
+            return NoContent();
+
+        }
+
+
+        //PETITION TO DELETE BY ID
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+
+            //find first the user
+            var userItem = await _dataContext.Users.FindAsync(id);
+            if (userItem == null)
+            {
+                return NotFound();
+            }
+
+            //Remove from DB and save the changes
+            _dataContext.Users.Remove(userItem);
+            await _dataContext.SaveChangesAsync();
+
+            //Just return a not content after delete
+            return NoContent();
 
         }
 
